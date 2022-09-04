e2fd1a0a38f0571233bd94c2fb8375cc21c99068
Aleksey <rgd666@yandex.ru>
2022-09-04 18:16:11 +0300

    добавили папку

--- additional_task/Task.py
+++ additional_task/Task.py
@@ -1,12 +1,26 @@
 # Последовательностью Фибоначчи называется последовательность чисел a0, a1, ..., an, ..., где
 # a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1).
 
+#fib1 = fib2 = 1
+#n = input('номер элемента ряда Фибоначи: ')
+#n = int(n)
+#
+#i = 0
+#while i < n - 2:
+#    fib_sum = fib1 + fib2
+#    fib1 = fib2
+#    fib2 = fib_sum
+#    i = i + 1
+#
+#print('Значение этого элемента: ', fib2)
+
+
+
 fib1 = fib2 = 1
-n = input('число Фибоначи: ')
-n = int(n) - 2
+n = int(input('номер элемента ряда Фибоначи: '))
 
-while n > 0:
+print(fib1, fib2, end =' ')
+
+for i in range (2, n):
     fib1, fib2 = fib2, fib1 + fib2
-    n -= 1
-
-print('Значение этого элемента: ', fib2)
+    print(fib2, end = ' ') 
